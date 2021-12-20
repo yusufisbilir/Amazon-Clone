1c81e7abce4b2363e4f920035fab3b033c5cd23a
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-21 00:52:35 +0300

    Create Navbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Navbar from './components/Navbar';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="h-16 bg-primary-gray flex justify-between items-center px-4 text-white">
+        {/* Nav Left */}
+        <div className="h-full flex items-center">
+          <a href="/">
+            <img src="/images/amazon_logo.png" alt="amazon" className="h-9" />
+          </a>
+          <div className="ml-6 flex">
+            <img
+              src="/images/location_icon.png"
+              alt="location"
+              className="h-4 mt-3 mr-1"
+            />
+            <a href="/">
+              <p className="leading-none text-gray-300 font-light text-xs">
+                Deliver to
+              </p>
+              <p className="font-semibold text-sm">Turkey</p>
+            </a>
+          </div>
+        </div>
+
+        {/* Nav search */}
+        <div className="flex flex-1 mx-12">
+          <input
+            type="text"
+            className="flex flex-1 h-10 rounded-l-md text-black p-3 focus:outline-yellow-500 focus:outline-2 border-none outline-none"
+          />
+          <button className="bg-yellow-500 px-3 rounded-r-md ">
+            <img src="/images/search_icon.png" alt="Search" className="h-6" />
+          </button>
+        </div>
+
+        {/* Nav Right */}
+        <div className="flex">
+          <a href="/">
+            <p className="text-xs">Hello, Sign in</p>
+            <p className="text-sm font-semibold">Account & Lists</p>
+          </a>
+
+          <a href="/" className="mx-6">
+            <p className="text-xs">Returns</p>
+            <p className="text-sm font-semibold">& Orders</p>
+          </a>
+          <a href="/" className="flex">
+            <img src="/images/cart_icon.png" alt="cart" className="h-9" />
+            <p className="text-sm mt-4">Cart</p>
+          </a>
+        </div>
+      </div>
+
+      {/* Links */}
+      <div className="bg-secondary-gray h-9 text-white flex items-center">
+        <div className="text-white text-sm">
+          <a href="/" className="ml-4">
+            All
+          </a>
+
+          <a href="/" className="ml-4">
+            Today's Deals
+          </a>
+          <a href="/" className="ml-4">
+            Customer Service
+          </a>
+          <a href="/" className="ml-4">
+            Registry
+          </a>
+          <a href="/" className="ml-4">
+            Gift Cards
+          </a>
+          <a href="/" className="ml-4">
+            Sell
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
