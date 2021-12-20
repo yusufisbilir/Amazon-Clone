fc758b9e5410f258574d088c52bdb40efda0a71d
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-21 00:29:42 +0300

    Reset css. Add main colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-gray': '#131921',
+        'secondary-gray': '#232f3e',
+      },
+    },
   },
   plugins: [],
 };
