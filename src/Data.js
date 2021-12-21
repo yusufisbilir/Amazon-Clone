f30f412b0956ca8f8dbf94ac294b7f7c4dca4ea0
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-21 11:21:56 +0300

    Add fake data.

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Macbook Air',
+    image:
+      'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=452&hei=420&fmt=jpeg&qlt=95&.v=1633027804000',
+
+    price: 999.0,
+    rating: 4.5,
+  },
+  {
+    _id: '2',
+    name: 'Ipad Pro',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-pro-12-11-select-202104?wid=545&hei=550&fmt=jpeg&qlt=95&.v=1617067380000',
+
+    price: 799.0,
+    rating: 4.0,
+  },
+  {
+    _id: '3',
+    name: 'iPhone SE',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-se-family-select-2020?wid=441&hei=529&fmt=jpeg&qlt=95&.v=1586794486444',
+
+    price: 400.99,
+    rating: 3,
+  },
+  {
+    _id: '4',
+    name: 'Apple Watch 7',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ML7Y3_VW_34FR+watch-45-alum-green-nc-7s_VW_34FR_WF_CO?wid=375&hei=356&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1629920460000%2C1631661629000',
+
+    price: 499.99,
+    rating: 5,
+  },
+  {
+    _id: '5',
+    name: 'AirPods',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MME73?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861342000',
+
+    price: 179.99,
+    rating: 3.5,
+  },
+  {
+    _id: '6',
+    name: 'Apple Tv 4k',
+    image:
+      'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/apple-tv-4k-hero-select-202104?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1619139498000',
+
+    price: 179.99,
+    rating: 4,
+  },
+];
+
+export default products;
