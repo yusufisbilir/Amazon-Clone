a80bf1abcdf2800f66757a0f29a113be69ff2994
yusufisbilir <dev.yusufisbilir@gmail.com>
2021-12-21 14:01:14 +0300

    Configure React Router. Add routes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import Order from './components/Order';
+import Cart from './components/Cart';
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="order" element={<Order />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
